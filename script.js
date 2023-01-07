507c2f4152f86b783e13dc15b82d99e2875c3d7e
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-01-07 18:26:57 +0100

    css

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+
+let ticketPrice = +movieSelect.value;
+
+container.addEventListener("click", (e) => {
+  console.log(e.target);
+});
