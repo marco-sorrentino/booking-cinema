0589465a6aaab7fb93c75c74a6c1604d864e6da6
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-01-07 18:45:37 +0100

    function on main container done

--- script.js
+++ script.js
@@ -3,8 +3,21 @@ const seats = document.querySelectorAll(".row .seat:not(.occupied");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
-
 let ticketPrice = +movieSelect.value;
+
+const updateSelectedCount = () => {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+};
+
+// movie select event
+
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount;
+});
 
 container.addEventListener("click", (e) => {
   if (
@@ -13,4 +26,6 @@ container.addEventListener("click", (e) => {
   ) {
     e.target.classList.toggle("selected");
   }
+
+  updateSelectedCount();
 });
